f382074aa8c2207f1fafee64e2458f993604c987
Mario <mario.mai@stud.uni-due.de>
2024-11-07 20:58:09 +0100

    Flexible Grid größe, 3 Farben und Eraser auswählbar

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 const map = document.getElementById("sixteenXsixteen");
 
+let color = "";
+
+
+document.getElementById("btnBlack").addEventListener("click", ()=>{
+    color = "black";
+})
+document.getElementById("btnOrange").addEventListener("click", ()=>{
+    color = "orangered";
+})
+document.getElementById("btnPink").addEventListener("click", ()=>{
+    color = "pink";
+})
+document.getElementById("btnEraser").addEventListener("click", ()=>{
+    color = "transparent";
+})
+
+
+
 function createMap(column, row){
     //Ein Div- row erstellt
     for(let rIndex = 0; rIndex < row; rIndex++ ){
@@ -10,10 +28,27 @@ function createMap(column, row){
     for (let cIndex = 0; cIndex < column; cIndex++) {
         const cell = document.createElement("div");
         cell.classList.add("column");
+
+
+        //Mouseover EventListener
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor= color;
+            console.log(color);
+        });
+
+
         rowElement.appendChild(cell);
         }
         map.appendChild(rowElement);
        
     }
 }
-createMap(16,16);
+
+document.getElementById("gridForm").addEventListener("submit", function(event){
+    event.preventDefault();
+
+    const columns = parseInt(document.getElementById("columns").value, 10);
+    const rows = parseInt(document.getElementById("rows").value, 10);
+
+    createMap(columns, rows);
+})
