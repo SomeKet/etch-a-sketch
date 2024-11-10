c2590da46b0d2b5f4d3b57ba6a0cbcdaeb83f069
Mario <mario.mai@stud.uni-due.de>
2024-11-10 14:14:55 +0100

    Coloring bug refactor und Eraser Logik implementiert

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const gridMap = document.querySelector('.gridMap');
 const sizeBtn = document.querySelector('.sbmBtn');
 const eraserBtn = document.querySelector('.eraser');
-const colorPara = document.querySelector('.colorRGBA');
+const colorPick = document.getElementById('colorId');
+const eraserFlag ={current: false};
+
 
 
 sizeBtn.addEventListener('click', function(e){
@@ -9,6 +11,14 @@ sizeBtn.addEventListener('click', function(e){
     const size = document.getElementById("sizeInput").value;
     createMap(size);
 
+})
+
+//Flag setzen für Farbe oder Radiergummi
+eraserBtn.addEventListener('click', ()=>{
+    eraserFlag.current = true;
+})
+colorPick.addEventListener('click', ()=>{
+    eraserFlag.current = false;
 })
 
 
@@ -19,7 +29,7 @@ function createMap(pixel) {
     for (let index = 0; index < pixel ** 2; index++) {
         const cell = document.createElement("div");
         cell.classList.add("cell");
-        cell.setAttribute('style', `text-align:center; width:${size}; height:${size}; background-color:white`);
+        cell.setAttribute('style', `text-align:center; width:${size}; height:${size}; background-color:transparent`);
         
         coloring(cell);
         gridMap.appendChild(cell);
@@ -28,22 +38,30 @@ function createMap(pixel) {
 
 function coloring(element){
     var opac = 1;
+    var prevColor = null;
+
     element.addEventListener('mouseover', ()=>{
-        const color = document.getElementById('colorId').value;
-
-        var red = parseInt(color.substring(1,3), 16);
-        var green = parseInt(color.substring(3,5), 16);
-        var blue = parseInt(color.substring(5,7), 16);
-    
-        if(opac < 11){
-            var rgba = `rgba(${red}, ${green}, ${blue}, ${opac/10})`;
-            element.style.backgroundColor = `rgba(${red}, ${green}, ${blue}, ${opac/10})`;
-            console.log(rgba);
-            opac ++;
+        if(eraserFlag.current == true){
+            element.style.backgroundColor = "transparent";
         }else{
-            console.log("finish")
+            //Farbe aus Input
+            const color = colorPick.value;
+    
+            //color in RGB parsen
+            var red = parseInt(color.substring(1,3),16);
+            var green = parseInt(color.substring(3,5),16);
+            var blue = parseInt(color.substring(5,7),16);
+    
+            //Abfrage ob neue Farbe eintritt, damit Element resetet wird
+            if(prevColor !== color){
+                opac = 1;
+                prevColor = color;
+            }
+    
+            if(opac <= 10){
+                element.style.backgroundColor = `rgba(${red},${green},${blue},${opac/10})`;
+                opac ++;
+            }
         }
-    })
-
-  
+    })      
 }
